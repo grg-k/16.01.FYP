77513d09cfdac68d10ab28a0c533a4f33fe41490
Georges Khneisser <96982193+grg-k@users.noreply.github.com>
2026-01-19 18:52:14 +0200

    Add files via upload

---
+++ 07. GOOGLE ADK TEST 2/multi_tool_agent/agent3.py
@@ -0,0 +1,19 @@
+from google.adk.agents import Agent
+from toolbox_core import ToolboxSyncClient
+
+MODEL_BASE_URL = ("http://vllm-test-llama3.murex.com/v1/chat/completions")
+MODEL_API_KEY = ("sk-ew@X1s!XXmaJ")
+
+toolbox = ToolboxSyncClient("http://localhost:8080/sse")
+
+tools = toolbox.load_tool('get_coordinates', 'get_weather', 'get_city_weather')
+
+root_agent = Agent(
+    model="llama3-8b",
+    name="weather_mcp_client_agent",
+    instruction=("Use the tools available to you (especially get_city_weather) to fetch weather data from cities the user asks for."),
+    tools=tools,
+    base_url=MODEL_BASE_URL,
+    api_key=MODEL_API_KEY,
+    headers={"Authorization": f"Bearer {MODEL_API_KEY}"},
+)
